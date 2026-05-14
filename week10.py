f75f6e69b48b2f4e93d6d00ec19f9e0b12be1258
LEEJAEHO98 <2098win@naver.com>
2026-05-14 15:35:54 +0900

    11주차

--- week10.py
+++ week10.py
@@ -1,11 +1,15 @@
 import pandas as pd
-import seaborn as sns
 
+# df1 = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8]}, index=[1, 2, 3])
+# df2 = pd.DataFrame({'국':[9, 6, 17], '수':[3, 5, 9]}, index=[1, 2, 4])
+# print(pd.merge(df1, df2, how='left', on='국'))
 
-mpg = sns.load_dataset('mpg')
-print(mpg[mpg['horsepower'].isnull()])
-mpg['horsepower'] = mpg['horsepower'].fillna(
-    mpg.groupby('cylinders')['horsepower'].transform('median')
-)
-print(mpg.info())
-print(mpg[mpg['horsepower'].isnull()])
+df = pd.DataFrame({'국':[7, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
+print(df)
+# print(df.describe())
+# print(df.info())
+# print(df['국'].value_counts())
+# print(len(df))
+# print(df.shape)
+print(df['국'].nunique())
+# print(df.dtypes)
